2c250ede697d90c5e07196abfedc754abe00f8da
João Victor Macedo Passos Rocha <jvr3292_undadm@3292SICOOB.LOCAL>
2023-01-19 12:20:02 -0300

    V1 - Interface desenvolvida

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,23 @@
+import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
+import * as Dialog from '@radix-ui/react-dialog'
+
+import logoImg from '../../assets/logo.svg'
+import { NewTransactionModal } from "../../pages/Transactions/components/NewTransactionModal";
+
+export function Header() {
+    return (
+        <HeaderContainer>
+            <HeaderContent>
+                <img src={logoImg} alt="" />
+
+                <Dialog.Root>
+                    <Dialog.Trigger asChild>
+                        <NewTransactionButton>Nova Transação</NewTransactionButton>
+                    </Dialog.Trigger>
+
+                    <NewTransactionModal />
+                </Dialog.Root>
+            </HeaderContent>
+        </HeaderContainer>
+    )
+}
